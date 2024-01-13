6654b6b278002de7497cf3f9150eb8eb110771b6
halalkharbouch <binyaminbgod@gmail.com>
2024-01-13 23:55:06 +0100

    Add create listing api route

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
 import cookieParser from "cookie-parser";
 
 
@@ -22,6 +23,7 @@ app.use(cookieParser());
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use('/api/listing', listingRouter);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
---
+++ api/models/listing.model.js
@@ -0,0 +1,119 @@
+import mongoose from "mongoose";
+
+const listingSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  size: {
+    type: Number,
+    required: true,
+  },
+  bedrooms: {
+    type: Number,
+    required: true,
+  },
+  bathrooms: {
+    type: Number,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  constructionYear: {
+    type: Number,
+    required: true,
+  },
+  renovationYear: {
+    type: Number,
+    required: true,
+  },
+  parking: {
+    type: Number,
+    required: true,
+  },
+  furnished: {
+    type: Boolean,
+    required: true,
+  },
+  additionalSpaces: {
+    type: String,
+  },
+  heating: {
+    type: String,
+    required: true,
+  },
+  airConditioning: {
+    type: Boolean,
+    required: true,
+  },
+  firePlace: {
+    type: Boolean,
+    required: true,
+  },
+  elevator: {
+    type: Boolean,
+    required: true,
+  },
+  ventilation: {
+    type: Boolean,
+    required: true,
+  },
+  intercom: {
+    type: Boolean,
+    required: true,
+  },
+  cableTv: {
+    type: Boolean,
+    required: true,
+  },
+  internet: {
+    type: Boolean,
+    required: true,
+  },
+  garage: {
+    type: Boolean,
+    required: true,
+  },
+  disabledAccess: {
+    type: String,
+    required: true,
+  },
+  petFriendly: {
+    type: Boolean,
+    required: true,
+  },
+  garden: {
+    type: Number,
+    required: true,
+  },
+  pool: {
+    type: Boolean,
+    required: true,
+  },
+  security: {
+    type: String,
+    required: true,
+  },
+  images: {
+    type: Array,
+    required: true,
+  },
+}, { timestamps: true });
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+export default Listing;
